de23592df4da43101237ecf95e754a0148154c1f
wacky-sjh <sjh@wacky.kr>
2025-06-16 17:01:10 +0900

    Fix: 사용자 안내 문구 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import ProductViewer from "./components/ProductViewer";
 
 function App() {
   return (
-    <>
+    <main
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <h1 style={{ textAlign: "center" }}>Product 3D Viewer</h1>
-      <div style={{ textAlign: "center" }}>
-        <p>마우스 드래그 / 터치로 회전</p>
-      </div>
+      <ul style={{ textAlign: "start", margin: "auto", padding: "0" }}>
+        <li>회전: DESKTOP - 마우스 드래그 | MOBILE - 터치</li>
+        <li>확대/축소: DESKTOP - 마우스 휠 | MOBILE - 터치</li>
+      </ul>
       <ProductViewer />
-    </>
+    </main>
   );
 }
 
