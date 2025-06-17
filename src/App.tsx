fb65fffe11fcad60454746056514ffa8954feab6
wacky-sjh <sjh@wacky.kr>
2025-06-17 10:41:10 +0900

    Feat: 탭 기능 및 3D 뷰어와 회전 상자 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import ProductViewer from "./components/ProductViewer";
+import RotationBox from "./components/RotationBox";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("product");
+
   return (
     <main
       style={{
@@ -10,12 +14,34 @@ function App() {
         alignItems: "center",
       }}
     >
-      <h1 style={{ textAlign: "center" }}>Product 3D Viewer</h1>
+      <h1 style={{ textAlign: "center" }}>3D Viewer</h1>
       <ul style={{ textAlign: "start", margin: "auto", padding: "0" }}>
         <li>회전: DESKTOP - 마우스 드래그 | MOBILE - 터치</li>
         <li>확대/축소: DESKTOP - 마우스 휠 | MOBILE - 터치</li>
       </ul>
-      <ProductViewer />
+      <div style={{ margin: "20px 0" }}>
+        <button
+          onClick={() => setSelectedTab("product")}
+          style={{
+            fontWeight: selectedTab === "product" ? "bold" : "normal",
+            marginRight: "10px",
+            padding: "10px 20px",
+          }}
+        >
+          Product
+        </button>
+        <button
+          onClick={() => setSelectedTab("box")}
+          style={{
+            fontWeight: selectedTab === "box" ? "bold" : "normal",
+            padding: "10px 20px",
+          }}
+        >
+          Box
+        </button>
+      </div>
+      {selectedTab === "product" && <ProductViewer />}
+      {selectedTab === "box" && <RotationBox />}
     </main>
   );
 }
